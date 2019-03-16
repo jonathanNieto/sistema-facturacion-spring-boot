ca288e39ec7a5c17fd64bcee5feafb2342533597
jonathanNieto <jonathan.nieto.davila@gmail.com>
2019-03-16 15:10:39 -0600

    create list view and add some clients in import sql file: class number 26

---
+++ facturacion-app/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO clients (id, name, lastname, email, create_at) VALUES(1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-28');
+INSERT INTO clients (id, name, lastname, email, create_at) VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clients (id, name, lastname, email, create_at) VALUES(3, 'Ashlynn', 'Sanford', 'Ike.Ward49@yahoo.com', '2017-08-28');
+INSERT INTO clients (id, name, lastname, email, create_at) VALUES(4, 'Nico', 'Gorczany','Shanny39@yahoo.com', '2017-08-28');
+INSERT INTO clients (id, name, lastname, email, create_at) VALUES(5, 'Heather', 'Quigley', 'Rusty_Shields42@yahoo.com', '2017-08-28');
+INSERT INTO clients (id, name, lastname, email, create_at) VALUES(6, 'Greta', 'Stehr', 'Lyla_Block29@example.org', '2017-08-28');
+INSERT INTO clients (id, name, lastname, email, create_at) VALUES(7, 'Leonor', 'Gottlieb', 'Margaret_Klein@example.net', '2017-08-28');
