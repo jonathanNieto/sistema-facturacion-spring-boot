1c7bb5eef1b8aace44220052e8655a9ee81cf6ca
jonathanNieto <jonathan.nieto.davila@gmail.com>
2019-03-18 15:38:50 -0600

    Add delete task for old client images: class number 57.

--- facturacion-app/src/main/java/com/jonatnie/facturacionapp/controller/ClientController.java
+++ facturacion-app/src/main/java/com/jonatnie/facturacionapp/controller/ClientController.java
@@ -1,5 +1,6 @@
 package com.jonatnie.facturacionapp.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
@@ -34,7 +35,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 /**
  * ClientController
  */
@@ -45,11 +45,12 @@ public class ClientController {
     @Autowired
     private IClientService clientService;
 
-    /* This is only for developing  TODO: remove before deploy*/
+    /* This is only for developing remove before deploy */
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    @GetMapping(value="/detail/{id}")
-    public String detail(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes redirectAttributes) {
+    @GetMapping(value = "/detail/{id}")
+    public String detail(@PathVariable(value = "id") Long id, Map<String, Object> model,
+            RedirectAttributes redirectAttributes) {
 
         Client client = clientService.findOne(id);
         if (client == null) {
@@ -62,7 +63,7 @@ public class ClientController {
         return "detail";
     }
 
-    @GetMapping(value="/upload/{filename:.+}")
+    @GetMapping(value = "/upload/{filename:.+}")
     public ResponseEntity<Resource> viewPhoto(@PathVariable String filename) {
         Path photoPath = Paths.get("upload").resolve(filename).toAbsolutePath();
         Resource resource = null;
@@ -75,11 +76,9 @@ public class ClientController {
             e.printStackTrace();
         }
         return ResponseEntity.ok()
-               .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-               .body(resource);
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
-    
-    
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String list(Model model) {
@@ -127,10 +126,23 @@ public class ClientController {
             return "form";
         }
         if (!photo.isEmpty()) {
+
+            if (client.getId() != null &&
+                client.getId() > 0 &&
+                client.getPhoto() != null &&
+                client.getPhoto().length() > 0) {
+                
+                Path rootPath = Paths.get("upload").resolve(client.getPhoto()).toAbsolutePath();
+                File file = rootPath.toFile();
+
+                if (file.exists() && file.canRead()) {
+                    file.delete();
+                }
+            }
             String uniqueFilename = UUID.randomUUID().toString() + "_" + photo.getOriginalFilename();
             Path rootPath = Paths.get("upload").resolve(uniqueFilename);
             Path rootAbsolutePath = rootPath.toAbsolutePath();
-            /* This is only for developing TODO: remove before deploy */
+            /* This is only for developing remove before deploy */
             log.info("rootPath: " + rootPath);
             log.info("rootAbsolutePath: "+ rootAbsolutePath);
                 try {
@@ -152,8 +164,18 @@ public class ClientController {
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String delete(@PathVariable(value = "id") Long id, RedirectAttributes redirectAttributes) {
         if (id > 0) {
+            Client client = clientService.findOne(id);
             clientService.delete(id);
             redirectAttributes.addFlashAttribute("success", "Cliente eliminado con éxito");
+
+            Path rootPath = Paths.get("upload").resolve(client.getPhoto()).toAbsolutePath();
+            File file = rootPath.toFile();
+
+            if (file.exists() && file.canRead()) {
+                if (file.delete()) {
+                   redirectAttributes.addFlashAttribute("info", "Foto " + client.getPhoto() + " de " + client.getName() + " eliminada con éxito");
+                }
+            }
         }
         return "redirect:/list";
     }
