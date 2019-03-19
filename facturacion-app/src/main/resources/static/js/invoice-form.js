8168371444e018d86092e64579862cb20f5bd455
jonathanNieto <jonathan.nieto.davila@gmail.com>
2019-03-19 15:00:16 -0600

    Update invoice form view and add autocomplete using jquery: class number 71.

--- facturacion-app/src/main/resources/static/js/invoice-form.js
+++ facturacion-app/src/main/resources/static/js/invoice-form.js
@@ -1,4 +1,3 @@
-
 var btnBack = document.querySelector("#btn-back");
 
 function goBack() {
@@ -6,3 +5,31 @@ function goBack() {
 }
 
 btnBack.addEventListener('click', goBack);
+
+/* jquery ui */
+$(document).ready(function () {
+    $("#find-product").autocomplete({
+        source: function (request, reponse) {
+            $.ajax({
+                url: "/invoice/load-products/" + request.term,
+                dataType: "jason",
+                data: {
+                    term: request.term
+                },
+                success: function (data) {
+                    Response($.map(data, function (item) {  
+                        return {
+                            value: item.id,
+                            label: item.name,
+                            cost: item.cost
+                        }
+                    }));
+                }
+            });
+        },
+        select: function (event, ui) { 
+            $("#find-product").val(ui.item.label);
+            return false;
+         }
+    });
+});
