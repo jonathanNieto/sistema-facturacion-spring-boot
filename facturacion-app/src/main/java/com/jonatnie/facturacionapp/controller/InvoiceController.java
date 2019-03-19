cb011e842f856595bfe44499331f2486f2edcca0
jonathanNieto <jonathan.nieto.davila@gmail.com>
2019-03-19 10:43:53 -0600

    Create invoice controller: class number 68.

---
+++ facturacion-app/src/main/java/com/jonatnie/facturacionapp/controller/InvoiceController.java
@@ -0,0 +1,50 @@
+package com.jonatnie.facturacionapp.controller;
+
+import java.util.Map;
+
+import com.jonatnie.facturacionapp.model.entity.Client;
+import com.jonatnie.facturacionapp.model.entity.Invoice;
+import com.jonatnie.facturacionapp.model.service.IClientService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+/**
+ * InvoiceController
+ */
+@Controller
+@RequestMapping("/invoice")
+@SessionAttributes("invoice")
+public class InvoiceController {
+
+    @Autowired
+    private IClientService clientService;
+
+    @GetMapping(value="/form/{clientId}")
+    public String create(@PathVariable(value = "clientId") Long id, 
+                        Map<String, Object> model, 
+                        RedirectAttributes redirectAttributes) {
+        Client client = clientService.findOne(id);
+        if (client == null ) {
+            redirectAttributes.addFlashAttribute("error", "Cliente no encontrado");
+            return "redirect:/list";
+        }
+
+        Invoice invoice = new Invoice();
+        invoice.setClient(client);
+
+        model.put("invoice", invoice);
+        model.put("title", "Formulario Factura");
+
+        return "invoice/form";
+    }
+    
+    
+}
