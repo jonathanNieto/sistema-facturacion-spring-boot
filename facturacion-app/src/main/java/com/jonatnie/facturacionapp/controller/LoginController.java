1d2e94fe9aeed429f1a961c1ea8c138efd640a8f
jonathanNieto <jonathan.nieto.davila@gmail.com>
2019-03-23 13:04:43 -0600

    Add error message to login controller: class number 100.

--- facturacion-app/src/main/java/com/jonatnie/facturacionapp/controller/LoginController.java
+++ facturacion-app/src/main/java/com/jonatnie/facturacionapp/controller/LoginController.java
@@ -5,8 +5,8 @@ import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 
 /**
  * LoginController
@@ -14,15 +14,18 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class LoginController {
 
-    @GetMapping(value="/login")
-    public String login(Model model, Principal principal, RedirectAttributes redirectAttributes) {
+    @GetMapping(value = "/login")
+    public String login(@RequestParam(value = "error", required = false) String error, Model model, Principal principal,
+            RedirectAttributes redirectAttributes) {
         if (principal != null) {
             redirectAttributes.addFlashAttribute("info", "Ya ha iniciado sesión anteriormente");
-            model.addAttribute("title", "Inicio de sesión");
             return "redirect:/";
         }
+        if (error != null) {
+            model.addAttribute("error", "Hay un error por favor verifique sus datos");
+        }
+        model.addAttribute("title", "Inicio de sesión");
         return "login";
     }
-    
-    
+
 }
