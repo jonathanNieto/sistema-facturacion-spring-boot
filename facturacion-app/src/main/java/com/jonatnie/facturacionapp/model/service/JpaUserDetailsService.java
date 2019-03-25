88c8bba610ce699f95dbc29b60bfb8c48fa8532d
jonathanNieto <jonathan.nieto.davila@gmail.com>
2019-03-25 08:58:54 -0600

    Create jpa user details service: class number 116.

---
+++ facturacion-app/src/main/java/com/jonatnie/facturacionapp/model/service/JpaUserDetailsService.java
@@ -0,0 +1,54 @@
+package com.jonatnie.facturacionapp.model.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.jonatnie.facturacionapp.model.dao.IUserDao;
+import com.jonatnie.facturacionapp.model.entity.Role;
+import com.jonatnie.facturacionapp.model.entity.User;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * JpaUserDetailsService
+ */
+@Service("jpaUserDetailsService")
+public class JpaUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private IUserDao userDao;
+
+    private Logger logger = LoggerFactory.getLogger(JpaUserDetailsService.class);
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDao.findByUsername(username);
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
+        if (user == null) {
+            logger.error("Error login: no existe usuario '" + username + "'");
+            throw new UsernameNotFoundException("Username: " + username + "no exitse en el sistema");
+        }
+        for (Role role : user.getRoleList()) {
+            authorities.add(new SimpleGrantedAuthority(role.getAuthority()));
+        }
+        if (authorities.isEmpty()) {
+            logger.error("Error login: usuario '" + username + "' no tiene roles asignados");
+            throw new UsernameNotFoundException("Username: " + username + " no tiene roles asignados");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.getEnabled(), true, true, true, authorities);
+    }
+
+    
+}
