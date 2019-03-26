34814c072dbdd6fab575b4e520b6f2298839fdf0
jonathanNieto <jonathan.nieto.davila@gmail.com>
2019-03-26 15:28:40 -0600

    Implement class client csv view and add link in list view to export csv: classes number 145 and 146.

--- facturacion-app/src/main/java/com/jonatnie/facturacionapp/view/csv/ClientCsvView.java
+++ facturacion-app/src/main/java/com/jonatnie/facturacionapp/view/csv/ClientCsvView.java
@@ -1,5 +1,6 @@
 package com.jonatnie.facturacionapp.view.csv;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,31 +8,40 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.jonatnie.facturacionapp.model.entity.Client;
 
-import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 /**
  * ClientCsvView
  */
-@Component("list")
+@Component("list.csv")
 public class ClientCsvView extends AbstractView {
 
     public ClientCsvView() {
         setContentType("text/csv");
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
             HttpServletResponse response) throws Exception {
 
-        MessageSourceAccessor messages = getMessageSourceAccessor();
-
-        Client client = (Client) model.get("client");
-        /* String filename = messages.getMessage("txt.client.list.invoice") + "_" + client.getName() + ".xlsx"; */
+        List<Client> clientList = (List<Client>) model.get("clients");
         String filename = "clients.csv";
         response.setHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
         response.setContentType(getContentType());
+
+        ICsvBeanWriter beanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+        String header[] = {"id", "name", "lastname", "email", "createAt"};
+
+        beanWriter.writeHeader(header);
+        for (Client client : clientList) {
+            beanWriter.write(client, header);
+        }
+        beanWriter.close();
     }
 
     @Override
